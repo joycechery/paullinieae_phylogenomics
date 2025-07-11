6b5a59cbca4b6335fe6e901b8bb3a83e2d68cee8
Joyce Gloria Onyenedum <joycegchery@gmail.com>
2025-07-11 19:37:50 -0400

    Update iqtree_bootstraps.sh

--- Tree_Inference/10.CHRONOGRAM/iqtree_bootstrapTrees/iqtree_bootstraps.sh
+++ Tree_Inference/10.CHRONOGRAM/iqtree_bootstrapTrees/iqtree_bootstraps.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Run IQ-TREE with specified parameters
+./iqtree2 \
+  -s pau_333s_351g.fasta \
+  -m GTR \
+  -bb 1000 \
+  -spp pau_333s_351g_partitions \
+  -t pau_333s_351g_partitions.contree \
+  -wbtl pau_333s_351g_partitions.BS_BL.tre
 
