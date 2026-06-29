5dd853db7b4e2fd87435cdef5dbe9e3a9f16396e
Joyce Gloria Onyenedum <joycegchery@gmail.com>
2026-06-29 16:21:55 -0400

    Update 5.Chronogram_Figure.R

--- Tree_Inference/10.CHRONOGRAM/treepl_chronogram_nosubsp./5.Chronogram_Figure.R
+++ Tree_Inference/10.CHRONOGRAM/treepl_chronogram_nosubsp./5.Chronogram_Figure.R
@@ -1,5 +1,5 @@
 ###############################################################################
-# Generate publication figure of chronogram (Updated for SumTrees)
+# Generate publication figure of chronogram 
 # By Joyce G. Onyenedum
 ###############################################################################
 
@@ -10,9 +10,9 @@ library(ggplot2)
 library(deeptime)
 
 ## TREE WITH NODE VALUES
-setwd("~/Desktop/")
+setwd("~/Desktop/10.CHRONOGRAM_Final/")
 # Load your new SumTrees file
-tree <- read.beast("final_summarized_chronogram.nex")
+tree <- read.beast("CI-combined_chronograms.nex")
 
 # Extract min and max values from the SumTrees 'age_hpd95' field safely
 tree@data$age_hpd95_min <- sapply(tree@data$age_hpd95, function(x) {
@@ -29,30 +29,31 @@ tree@data$age_hpd95_max <- sapply(tree@data$age_hpd95, function(x) {
 p <- ggtree(tree) +
   # Updated to use the SumTrees HPD variable name
   geom_range("age_hpd95", color = "steelblue", size = 1.7, alpha = 0.8) +
-  geom_tiplab(size = 1.5) +
-  geom_text2(
-    aes(label = ifelse(
-      !isTip & !is.na(age_mean) & age_mean > 0,
-      paste0(
-        sprintf("%.2f", age_mean), " [",
-        sprintf("%.2f", age_hpd95_min), "-",
-        sprintf("%.2f", age_hpd95_max), "]"
-      ),
-      NA
-    )),
-    hjust = -0.15,
-    size = .65,
-    na.rm = TRUE
-  ) +
-  # Adjust xlim if your root age (~68.8 Mya) is cut off by the geological timeline
-  coord_geo(
-    xlim = c(-75, 5), 
-    ylim = c(0, 230), # Adjusted ylim to fit screme
-    dat = list("epochs"),
-    abbrv = FALSE,
-    size = list(4, 3),
-    neg = TRUE
-  ) +
+  geom_tiplab(size = 1.8) +
+  #  geom_text2(
+  #    aes(label = ifelse(
+  #      !isTip & !is.na(age_mean) & age_mean > 0,
+  #      paste0(
+  #        sprintf("%.2f", age_mean), " [",
+  #        sprintf("%.2f", age_hpd95_min), "-",
+  #        sprintf("%.2f", age_hpd95_max), "]"
+  #      ),
+  #      NA
+  #    )),
+  #    hjust = -0.15,
+#    size = 1,
+#    na.rm = TRUE
+#  ) +
+# Adjust xlim if your root age (~68.8 Mya) is cut off by the geological timeline
+coord_geo(
+  xlim = c(-75, 10), 
+  ylim = c(0, 227), # Adjusted ylim to fit screme
+  dat = list("epochs"),
+  fill = list(c("grey90")),
+  abbrv = FALSE,
+  size = list(4, 3),
+  neg = TRUE
+) +
   scale_x_continuous(
     breaks = seq(-70, 0, 10),
     labels = abs(seq(-70, 0, 10))
@@ -61,3 +62,5 @@ p <- ggtree(tree) +
 
 # Reverse the time scale so 0 is present-day on the right
 revts(p)
+
+#exported 9 x 13
